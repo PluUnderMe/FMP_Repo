3e1f2bac6eb10065fe807e828961022531bc6ede
PluUnderMe <brad.soper@outlook.com>
2024-05-19 17:53:53 +0100

    Finished notes and added pickup script

--- Brad_FMP/Assets/Scipts/NotesSystem.cs
+++ Brad_FMP/Assets/Scipts/NotesSystem.cs
@@ -79,26 +79,43 @@ public class NotesSystem : MonoBehaviour
     }
     private int currentPage = 0;
     private bool readSubscript = false;
-    private Sprite defaultPageTeture = null;
+    private Sprite defaultPageTexture = null;
     private bool usingNoteSystem;
     #endregion
 
+    #region Unity's Default Methods
+
     private void OnEnable()
     {
-        
+        A_Display += DisplayNote;
     }
     private void OnDisable()
     {
-        
+        A_Display -= DisplayNote;
     }
     private void Start()
     {
-        
+        Close(false);
+        defaultPageTexture = UI.Page.sprite;
     }
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            usingNoteSystem = !usingNoteSystem;
+            switch (usingNoteSystem)
+            {
+                case true:
+                    Open();
+                    break;
+                case false:
+                    Close(activeNote != null);
+                    break;
+            }
+        }
     }
+
+    #endregion
 
     public void Open()
     {
@@ -107,8 +124,98 @@ public class NotesSystem : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
 
         UpdateList();
+        UpdateCanvasGroup(true, UI.ListCanvasGroup);
+
+    }
+    public void Close(bool playSFX)
+    {
+        UnityEngine.Object.FindObjectOfType<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        CloseNote(playSFX);
+        UpdateCanvasGroup(false, UI.ListCanvasGroup);
     }
     
+    private void DisplayNote(Note note)
+    {
+        if (note == null) { return; }
+
+        PlaySound(openNoteSFX);
+
+        UpdateCanvasGroup(true, UI.NoteCanvasGroup);
+        activeNote = note;
+
+        DisplayPage(0);
+    }
+    private void DisplayPage(int page)
+    {
+        UI.ReadButton.interactable = activeNote.Pages[page].Type == PageType.Texture;
+
+        if (activeNote.Pages[page].Type != PageType.Texture)
+        { readSubscript = false; } else { if (readSubscript) { UpdateSubscript(); } }
+
+        sources[1].Stop();
+        if (activeNote.Pages[page].Narration != null)
+        {
+            if (!activeNote.Pages[page].NarrationPlayed)
+            {
+                sources[1].clip = activeNote.Pages[page].Narration;
+                sources[1].Play();
+                if (activeNote.Pages[page].Narration_PlayOnce)
+                {
+                    activeNote.Pages[page].NarrationPlayed = true;
+                }
+
+            }
+        }
+
+        switch (activeNote.Pages[page].Type)
+        {
+            case PageType.Text:
+                UI.Page.sprite = defaultPageTexture;
+                UI.TextObj.text = activeNote.Pages[page].Text;
+                break;
+            case PageType.Texture:
+                UI.Page.sprite = activeNote.Pages[page].Texture;
+                UI.TextObj.text = string.Empty;
+                break;
+        }
+        UpdateUI();
+    }
+
+    public static void Display(Note note)
+    {
+        A_Display(note);
+    }
+    public static void Display(string key)
+    {
+        var note = GetNote(key);
+        A_Display(note);
+    }
+
+    public  void CloseNote(bool playSFX)
+    {
+        if (playSFX)
+        {
+            PlaySound(closeNoteSFX);
+        }
+
+        UpdateCanvasGroup(false, UI.NoteCanvasGroup);
+        OnNoteClose();
+    }
+
+    private void UpdateUI()
+    {
+        UI.PreviousButton.interactable = !(currentPage == 0);
+        UI.NextButton.interactable = !(currentPage == activeNote.Pages.Length - 1);
+
+        var useSubscript = ActivePage.Type == PageType.Texture && ActivePage.UseSubscript;
+        UI.ReadButton.alpha = useSubscript ? (readSubscript ? .5f : 1f) : 0f;
+        UpdateCanvasGroup(readSubscript, UI.SubscriptGroup);
+
+        UI.Lines.enabled = ActivePage.DisplayLines;
+    }
     private void UpdateList()
     {
         ClearList();
@@ -132,6 +239,31 @@ public class NotesSystem : MonoBehaviour
             index++;
         }
     }
+    private void UpdateSubscript()
+    {
+        UI.Subscript.text = readSubscript ? ActivePage.Text : string.Empty;
+    }
+
+
+    public void Next()
+    {
+        PlaySound(turnPageSFX);
+        currentPage++;
+        DisplayPage(currentPage);
+    }
+    public void Previous()
+    {
+        PlaySound(turnPageSFX);
+        currentPage--;
+        DisplayPage(currentPage);
+    }
+    public void Read()
+    {
+        readSubscript = !readSubscript;
+
+        UpdateSubscript();
+        UpdateUI();
+    }
 
     private void ClearList()
     {
@@ -140,5 +272,61 @@ public class NotesSystem : MonoBehaviour
             Destroy(note.gameObject);
         }
         noteDatas.Clear();
+    }
+    private void OnNoteClose()
+    {
+        activeNote = null;
+        currentPage = 0;
+        readSubscript = false;
+        sources[1].Stop();
+    }
+    private void UpdateCanvasGroup(bool state, CanvasGroup canvasGroup)
+    {
+        switch (state)
+        {
+            case true:
+                canvasGroup.alpha = 1.0f;
+                canvasGroup.blocksRaycasts = true;
+                canvasGroup.interactable = true;
+                break;
+            case false:
+                canvasGroup.alpha = 0.0f;
+                canvasGroup.blocksRaycasts = false;
+                canvasGroup.interactable = false;
+                break;
+
+        }
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip)
+        {
+            sources[0].PlayOneShot(clip);
+        }
+    }
+    private void PlaySound(AudioClip[] clips)
+    {
+        if (clips != null)
+        {
+            var sfx = clips[UnityEngine.Random.Range(0, clips.Length)];
+            sources[0].PlayOneShot(sfx);
+        }
+    }
+
+    public static void AddNote(string key, Note note)
+    {
+        if (Notes.ContainsKey(key) == false)
+        {
+            Notes.Add(key, note);
+        }
+    }
+    public static Note GetNote(string key)
+    {
+        if (Notes.ContainsKey(key))
+        {
+            return Notes[key];
+        }
+        return null;
     }
 }
